0fe996d36e21b1a2dfaa1982a900e884fb6c0f15
MateuszM <mateuszmysliwiec03@gmail.com>
2021-04-30 12:12:46 +0200

    I added lever ladder

--- src/main/indexPage.js
+++ src/main/indexPage.js
@@ -1,14 +1,1 @@
 import '../scss/main.scss';
-
-let languageButton = document.querySelector('.languageFlexButton');
-languageButton.addEventListener('click', () => {
-  let allProgrammingLanguages = document.querySelector('.allLanguages');
-  let choseLanguageIcon = document.querySelector('.choseLanguageIcon');
-  if (allProgrammingLanguages.style.display === 'none') {
-    allProgrammingLanguages.style.display = 'block';
-    choseLanguageIcon.style.transform = 'rotate(90deg)';
-  } else {
-    allProgrammingLanguages.style.display = 'none';
-    choseLanguageIcon.style.transform = 'rotate(0deg)';
-  }
-});
