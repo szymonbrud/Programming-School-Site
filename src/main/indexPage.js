4a6bababea49566b3c381643a4a323b079b4147f
MateuszM <mateuszmysliwiec03@gmail.com>
2021-05-02 19:58:00 +0200

    Added levels without clickable divs. Only static rectangle and this its a retro levels because you must first chose an a programming language.

--- src/main/indexPage.js
+++ src/main/indexPage.js
@@ -37,6 +37,46 @@ const editListOfProgrammingHTML = (list) => {
 
       languagesWithoutSelectedOption.splice(indexToDelete, 1);
       editListOfProgrammingHTML(languagesWithoutSelectedOption);
+      //początek
+
+      let levels = document.querySelector('.levels');
+      levels.innerHTML = '';
+      fetch(secrets.api, {
+        method: 'POST',
+        body: JSON.stringify({
+          query: `query{
+              offersByLvls{
+                id,
+                level,
+                programmingLanguage,
+                willLearn
+              }
+          }`,
+          variables: { id: 1 },
+        }),
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+          res.data.offersByLvls.forEach((arg) => {
+            let allText = '';
+            const text = arg.willLearn.split('\n');
+            text.forEach((textElement) => {
+              allText += `<p class="willLearn">-${textElement}</p>`;
+            });
+            if (languages[indexToDelete] === arg.programmingLanguage) {
+              levels.innerHTML +=
+                '<div class="contentContainer"><p class = "level">' +
+                arg.level +
+                '</p>' +
+                '<p>' +
+                allText +
+                '</p>';
+            }
+          });
+        })
+        .catch((e) => Error(e));
+      //koniec
     });
   });
 };
@@ -99,45 +139,3 @@ window.addEventListener('load', () => {
   contactForm();
   menuPhone();
 });
-
-fetch(secrets.api, {
-  method: 'POST',
-  body: JSON.stringify({
-    query: `query{
-        offersByLvls{
-          id,
-          level,
-          programmingLanguage,
-          willLearn
-        }
-    }`,
-    variables: { id: 1 },
-  }),
-})
-  .then((res) => res.json())
-  .then((res) => {
-    console.log(res);
-    res.data.offersByLvls.forEach((arg) => {
-      console.log(arg.programmingLanguage);
-
-      //1 pogrupować na języki[]
-      //zrobić divy danych poziomów
-      //po zrobieniu kontenera iterowaćpo liniach
-
-      let levels = document.querySelector('.levels');
-      let allText = '';
-      const text = arg.willLearn.split('\n');
-      text.forEach((textElement) => {
-        console.log(textElement);
-        allText += `<p class="willLearn">-${textElement}</p>`;
-      });
-      levels.innerHTML =
-        '<div class="contentContainer"><p class = "level">' +
-        arg.level +
-        '</p>' +
-        '<p>' +
-        allText +
-        '</p>';
-    });
-  })
-  .catch((e) => Error(e));
